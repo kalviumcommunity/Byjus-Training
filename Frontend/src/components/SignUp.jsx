de62b4011eda3b5af7841e75991a3aef58c22306
jawahar-lee03 <jawahar.lingam@kalvium.community>
2023-07-04 15:48:38 +0530

    Added Signup Page
    - Created a form using react form
    - Form validation using formik library
    - Added a google Login component which will handle OAuth verification
    - Minor bug fixes in home component

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -1,7 +1,371 @@
+import { Box, Button, Flex, Text, Image } from "@chakra-ui/react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import backgroundImage from "../assets/Background-img.png";
+import { GoogleButton } from "./GoogleLogin";
+import "../form.css"
+
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+
+    const initialValues = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
+
+    const validate = (values) => {
+        const errors = {};
+        if (!values.firstName) {
+            errors.firstName = "Required*";
+        }
+        if (!values.lastName) {
+            errors.lastName = "Required*";
+        }
+        if (!values.email) {
+            errors.email = "Required*";
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+            errors.email = "Invalid email address";
+        }
+        if (!values.password) {
+            errors.password = "Required*";
+        } else if (values.password.length < 8) {
+            errors.password = "Password must be at least 8 characters long";
+        }
+        if (!values.confirmPassword) {
+            errors.confirmPassword = "Confirm your password";
+        } else if (values.password !== values.confirmPassword) {
+            errors.confirmPassword = "Passwords do not match";
+        }
+        return errors;
+    };
+
+    const onSubmit = (values, { setSubmitting }) => {
+        setTimeout(() => {
+            console.log(values);
+            setSubmitting(false);
+        }, 400);
+    };
+
+    return (
+        <>
+            <Image
+                src={backgroundImage}
+                alt="Background"
+                objectFit="cover"
+                position="absolute"
+                top={0}
+                left={0}
+                width="100vw"
+                height="100vh"
+                zIndex={-2}
+            />
+            <Box
+                bgColor="black"
+                h="100vh"
+                w="100vw"
+                opacity="40%"
+                position="fixed"
+                zIndex="-998"
+                overflow="hidden"
+            />
+            <Flex
+                h="100vh"
+                w="100vw"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Box
+                    h="560px"
+                    w="580px"
+                    borderRadius="28px"
+                    backgroundColor="#E8F4F9"
+                    position="relative"
+                >
+                    <Box>
+                        <Text
+                            className="heading"
+                            fontWeight="bold"
+                            textAlign="center"
+                            marginTop="60px"
+                        >
+                            Create a new account
+                        </Text>
+                        <Formik
+                            initialValues={initialValues}
+                            onSubmit={onSubmit}
+                            validate={validate}
+                        >
+                            {({
+                                values,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+                                <Form onSubmit={handleSubmit}>
+                                    <Flex
+                                        margin="30px 30px 8px 30px"
+                                        flexDirection="column"
+                                        gap="4px"
+                                    >
+                                        <Flex
+                                            justifyContent="space-between"
+                                            flexDir="row"
+                                            margin="4px 0px"
+                                        >
+                                            <Flex
+                                                alignContent="center"
+                                                justifyContent="flex-end"
+                                                flexDirection="column"
+                                            >
+                                                <Box className="container">
+                                                    <Field
+                                                        type="text"
+                                                        name="firstName"
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        value={values.firstName}
+                                                        className="input s-input"
+                                                        required={true}
+                                                    />
+                                                    <label
+                                                        htmlFor="firstName"
+                                                        className="label x-small"
+                                                    >
+                                                        First Name
+                                                    </label>
+                                                </Box>
+                                                <Box className="errorMessage">
+                                                    <ErrorMessage
+                                                        name="firstName"
+                                                        component="span"
+                                                    />
+                                                </Box>
+                                            </Flex>
+
+                                            <Flex
+                                                alignContent="center"
+                                                justifyContent="flex-end"
+                                                flexDirection="column"
+                                            >
+                                                <Box className="container">
+                                                    <Field
+                                                        width="300px"
+                                                        type="text"
+                                                        name="lastName"
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        value={values.lastName}
+                                                        className="input s-input"
+                                                        required={true}
+                                                    />
+                                                    <label
+                                                        htmlFor="lastName"
+                                                        className="label x-small"
+                                                    >
+                                                        Last Name
+                                                    </label>
+                                                </Box>
+                                                <Box className="errorMessage">
+                                                    <ErrorMessage
+                                                        name="lastName"
+                                                        component="span"
+                                                    />
+                                                </Box>
+                                            </Flex>
+                                        </Flex>
+
+                                        <Flex
+                                            alignContent="center"
+                                            justifyContent="flex-end"
+                                            flexDirection="column"
+                                        >
+                                            <Box className="container">
+                                                <Field
+                                                    type="email"
+                                                    name="email"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.email}
+                                                    className="input l-input"
+                                                    required={true}
+                                                />
+                                                <label
+                                                    htmlFor="email"
+                                                    className="label x-small"
+                                                >
+                                                    Email
+                                                </label>
+                                            </Box>
+                                            <Box className="errorMessage">
+                                                <ErrorMessage
+                                                    name="email"
+                                                    component="span"
+                                                />
+                                            </Box>
+                                        </Flex>
+
+                                        <Flex
+                                            justifyContent="space-between"
+                                            flexDir="row"
+                                            margin="4px 0px"
+                                        >
+                                            <Flex
+                                                alignContent="center"
+                                                justifyContent="flex-end"
+                                                flexDirection="column"
+                                            >
+                                                <Box className="container">
+                                                    <Field
+                                                        type="password"
+                                                        name="password"
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        value={values.password}
+                                                        className="input s-input"
+                                                        required={true}
+                                                    />
+                                                    <label
+                                                        htmlFor="password"
+                                                        className="label x-small"
+                                                    >
+                                                        Password
+                                                    </label>
+                                                </Box>
+                                                <Box className="errorMessage">
+                                                    <ErrorMessage
+                                                        name="password"
+                                                        component="span"
+                                                    />
+                                                </Box>
+                                            </Flex>
+
+                                            <Flex
+                                                alignContent="center"
+                                                justifyContent="flex-end"
+                                                flexDirection="column"
+                                            >
+                                                <Box className="container">
+                                                    <Field
+                                                        type="password"
+                                                        name="confirmPassword"
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        value={
+                                                            values.confirmPassword
+                                                        }
+                                                        className="input s-input"
+                                                        required={true}
+                                                    />
+                                                    <label
+                                                        htmlFor="confirmPassword"
+                                                        className="label x-small"
+                                                    >
+                                                        Confirm Password
+                                                    </label>
+                                                </Box>
+                                                <Box className="errorMessage">
+                                                    <ErrorMessage
+                                                        name="confirmPassword"
+                                                        component="span"
+                                                    />
+                                                </Box>
+                                            </Flex>
+                                        </Flex>
+                                    </Flex>
+
+                                    <Flex
+                                        margin="0px 30px"
+                                        alignItems="center"
+                                        justifyContent="center"
+                                    >
+                                        <Button
+                                            type="submit"
+                                            disabled={isSubmitting}
+                                            height="40px"
+                                            width="120px"
+                                            borderRadius="4px"
+                                            bgColor="#0F0F0F"
+                                            color="#FFFFFF"
+                                            className="small"
+                                            fontWeight="bold"
+                                            border="1px solid #FFFFFF"
+                                            boxShadow="-2px 4px 6px rgba(0, 0, 0, 0.5)"
+                                            _hover={{
+                                                cursor: "pointer",
+                                                color: "#E5FFE4",
+                                            }}
+                                            _active={{
+                                                boxShadow:
+                                                    "-0px 2px 6px rgba(0, 0, 0, 0.5)",
+                                                transform:
+                                                    "translateY(1px) translateX(-1px)",
+                                            }}
+                                        >
+                                            Sign up
+                                        </Button>
+                                    </Flex>
+                                </Form>
+                            )}
+                        </Formik>
+                        <Flex
+                            height="160px"
+                            width="100%"
+                            flexDirection="column"
+                            position="absolute"
+                            bottom="12px"
+                            justifyContent="space-around"
+                            alignItems="center"
+                        >
+                            <Flex
+                                flexDirection="row"
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                <Box
+                                    height="1px"
+                                    width="200px"
+                                    backgroundColor="#4A4A4A"
+                                ></Box>
+                                <Text
+                                    margin="8px"
+                                    fontWeight="bold"
+                                    className="x-small"
+                                    color="#4A4A4A"
+                                >
+                                    or
+                                </Text>
+                                <Box
+                                    height="1px"
+                                    width="200px"
+                                    backgroundColor="#4A4A4A"
+                                ></Box>
+                            </Flex>
+                            {<GoogleButton />}
+                            <Box color="#4A4A4A" className="x-small">
+                                Already a user?
+                                <a href="./signIn">
+                                    <Text
+                                        className="x-small"
+                                        fontWeight="bold"
+                                        display="inline"
+                                        margin="6px"
+                                        color="#6443AB"
+                                        textDecoration="underline"
+                                    >
+                                        SignIn
+                                    </Text>
+                                </a>
+                            </Box>
+                        </Flex>
+                    </Box>
+                </Box>
+            </Flex>
+        </>
+    );
 }
 
-export default SignUp
+export default SignUp;
